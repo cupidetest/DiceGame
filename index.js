72aeb77d9b08135b82f91c7428bf37ffb5c987e8
sneha <105018436+cupidetest@users.noreply.github.com>
2023-09-01 02:00:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var randomVariable1=(Math.floor(Math.random()*6) + 1 );
+var randomVariable2=(Math.floor(Math.random()*6) + 1);
+console.log(randomVariable1);
+console.log(randomVariable2);
+
+var randomDiceImage1="dice" + randomVariable1 +".png";
+var randomImg1="images/" + randomDiceImage1;
+
+var randomDiceImage2="dice" + randomVariable2 +".png";
+var randomImg2="images/" + randomDiceImage2;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImg1);
+document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
+
+if(randomVariable1>randomVariable2)
+{
+    document.querySelector("h1").innerHTML="Left wing wins :>";
+}
+else if(randomVariable2>randomVariable1)
+{
+    document.querySelector("h1").innerHTML="Right wing wins :>";
+}
+else
+{
+    document.querySelector("h1").innerHTML="DRAW, roll again :>";
+}
+
